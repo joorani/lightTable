0c7c1d5ab29d293057cfee8a2f9331fba379189d
rkd1326@naver.com <hanghaerkd1326>
2021-11-04 14:49:57 +0900

    ...

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
-
 import requests
 
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from bson.json_util import loads, dumps
-import jwt
-import datetime
-import hashlib
+import jwt, datetime, hashlib
+from datetime import datetime, timedelta
 
 from pymongo import MongoClient
-from datetime import datetime, timedelta
 
 app = Flask(__name__)
 # git에 올라가면 안되는 내용!
-# app.config["TEMPLATES_AUTO_RELOAD"] = True
-# app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+#app.config["TEMPLATES_AUTO_RELOAD"] = True
+#app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
-# SECRET_KEY = 'SPARTA'
+#SECRET_KEY = 'SPARTA'
 
-client = MongoClient('3.35.48.69', 27017, username="light", password="table")
-db = client.collections
+client = MongoClient('localhost', 27017)
+db = client.projectdb
+collection = db.collection
 
 
-# doc = {'img_url': 'https://recipe1.ezmember.co.kr/cache/recipe/2018/10/29/0dd4c2525a4585417e37cff57a7701d11_m.jpg',
-#        'title': '돼지고기조림', 'ingredients':'돼지고기, 간장','recipe':'곤약 넣고 조리기..'}
-# db.project1.insert_one(doc)
 
 @app.route('/')
 def main():
@@ -41,6 +36,7 @@ def home():
 
         user_info = db.users.find_one({"userid": payload["userid"]})
         return render_template('index.html', user_info=user_info)
+        # 로그인시간 만료, 정보 에러시 보여주는 페이지 생성해야함. index.html?? url_for에 연결.
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
@@ -97,7 +93,7 @@ def check_dup():
 ## 메인페이지
 # db에서 재료 가져오기
 @app.route('/detail/<keyword>')
-def detail(keyword):
+def detail1(keyword):
     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token [내토큰]"})
     result = r.json()
     print(result)
@@ -127,44 +123,81 @@ def recipecards():
     recipeInfo = list(db.project1.find({'ingredients': {'$regex': ingredientsData}}, {'_id': 0}))
     return render_template("main.html", recipeInfo=recipeInfo)
 
-## 상세페이지(리뷰도 보여줘야되기 때문에 로그인한 사람만 볼 수 있음)
-@app.route("/detail/<recipe_title>")
-def detail(recipe_title):
+## 상세페이지
+# db에서 제목 가져오기(jinja2)
+@app.route('/detail')
+def title():
+    # DB에서 저장된 title 찾아서 main title에 나타내기
+    title = list(db.레시피db.find({}))
+    return render_template("detail.html", title=title)
+
+# 상세페이지에 제목가져오기
+@app.route('/detail', methods=['GET'])
+def show_detailWin():
+    titleData = request.args.get('title')
+    getdetailWin = list(db.레시피db.find({'title': {'$regex': titleData}}, {'_id': 0})) #db 확인
+    return jsonify({'getdetailWin': getdetailWin})
+
+# //////////////////////////댓글을 db에 저장하기
+@app.route('/posting', methods=['POST'])
+def posting():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        userid = (payload["id"])
+        user_info = db.users.find_one({"username": payload["id"]})
+        comment_receive = request.form["comment_give"]
+        date_receive = request.form["date_give"]
+        print(type(date_receive))
+        doc = {
+            "username": user_info["username"],
+            "profile_name": user_info["profile_name"],
+            "profile_pic_real": user_info["profile_pic_real"],
+            "comment": comment_receive,
+            "date": date_receive
+        }
+        db.posts.insert_one(doc)
+        return jsonify({"result": "success", 'msg': '포스팅 성공'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
 
-        detail_recipe = db.recipe.find_one({'title': recipe_title}, {'_id': 0})
+# 댓글 클라이언트로 보내주기 / //////////////////////
+@app.route("/get_posts", methods=['GET'])
+def get_posts():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        posts = list(db.posts.find({}).sort("date", -1).limit(100))
+        for post in posts:
+            post["_id"] = str(post["_id"])
+            post["count_heart"] = db.likes.count_documents({"post_id": post["_id"], "type": "heart"})
+            post["heart_by_me"] = bool(db.likes.find_one({"post_id": post["_id"], "type": "heart", "username": payload['id']}))
+        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts": posts})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
 
-        pickedList = db.users.find_one({'userid': userid}, {'_id': 0})
-        reviews = list(db.comments.find({'title': recipe_title))
-        return render_template("detail.html", recipe=detail_recipe, comments=pickedList)
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
-# 코멘트 달기
-@app.route('/comment', methods=['POST'])
-def save_order():
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
-
-    doc = {
-        'name': name_receive,
-        'comment': comment_receive,
-    }
-
-    print(doc)
-    db.commetndb.insert_one(doc) #db 확인
-    return jsonify({'msg': '등록 완료'})
-
-# comment 목록보기(Read) API
-@app.route('/comment', methods=['GET'])
-def view_orders():
-    commnetList = list(db.commetndb.find({},{'_id':False})) #db 확인
-    return jsonify({'commentList':commnetList})
+# /////////////////좋아요 받아서 저장하기
+@app.route('/update_like', methods=['POST'])
+def update_like():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        post_id_receive = request.form["post_id_give"]
+        type_receive = request.form["type_give"]
+        action_receive = request.form["action_give"]
+        doc = {
+            "post_id": post_id_receive,
+            "username": user_info["username"],
+            "type": type_receive
+        }
+        if action_receive == "like":
+            db.likes.insert_one(doc)
+        else:
+            db.likes.delete_one(doc)
+        count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})
+        return jsonify({"result": "success", 'msg': 'updated', "count": count})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
